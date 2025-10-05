2b2b81629d12c54d8fadf39e88437c07c953a48f
Altay Brusan <altay_bn@yahoo.com>
2025-10-05 11:13:09 +0300

    Refactor PacsNode and update TechniqueParameter
    Refactored the `PacsNode` class in `PacsNode.h` for improved readability by restructuring member variables while retaining functionality. Reformatted the `TechniqueParameter` class in `TechniqueParameter.h` and added a new `BodyPart` member variable to represent the anatomical body part associated with the technique. Default values for existing members remain unchanged.

--- Common/Include/Pacs/Data/Entity/PacsNode.h
+++ Common/Include/Pacs/Data/Entity/PacsNode.h
@@ -10,16 +10,15 @@
 using namespace Etrek::Pacs;
 namespace Etrek::Pacs::Data::Entity {
 
-class PacsNode {
-public:
-  int Id = -1; ///< Primary key
-  PacsEntityType Type =
-      PacsEntityType::Archive; ///< Type of PACS entity (Archive, MPPS, etc.)
-  QString HostName;            ///< short heading of the comment
-  QString HostIp;              ///< full image note
-  int Port = 0;                ///< Port number
-  QString CalledAet;           ///< is reject comment flag
-  QString CallingAet;
+    class PacsNode {
+    public:
+        int Id = -1;                                    ///< Primary key       
+        PacsEntityType Type = PacsEntityType::Archive;  ///< Type of PACS entity (Archive, MPPS, etc.)
+        QString HostName;                               ///< short heading of the comment
+        QString HostIp;                                 ///< full image note
+        int Port = 0;                                   ///< Port number
+        QString CalledAet;                              ///< is reject comment flag
+        QString CallingAet;
 
   PacsNode() = default;
 
