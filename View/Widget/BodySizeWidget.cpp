074543311e5593f4566d2e629dad442c3f77498b
altay-brusan <altay_bn@yahoo.com>
2025-12-07 14:58:35 +0300

    Add patient size handling to BodySizeWidget
    Introduced a `PatientSize` enum to represent patient sizes (`Infant`, `Small`, `Normal`, `Large`) and added methods to set and get the selected size.
    
    Connected radio button toggles to emit a new `patientSizeChanged` signal, enabling notification of size changes. Defaulted the size to `Normal` if no selection is made.

--- View/Widget/BodySizeWidget.cpp
+++ View/Widget/BodySizeWidget.cpp
@@ -6,9 +6,52 @@ BodySizeWidget::BodySizeWidget(QWidget *parent)
     , ui(new Ui::BodySizeWidget)
 {
     ui->setupUi(this);
+
+    // Connect radio buttons to emit patientSizeChanged signal
+    connect(ui->infantRadioButton, &QRadioButton::toggled, this, [this](bool checked) {
+        if (checked) emit patientSizeChanged(PatientSize::Infant);
+    });
+    connect(ui->smallRadioButton, &QRadioButton::toggled, this, [this](bool checked) {
+        if (checked) emit patientSizeChanged(PatientSize::Small);
+    });
+    connect(ui->normalRadioButton, &QRadioButton::toggled, this, [this](bool checked) {
+        if (checked) emit patientSizeChanged(PatientSize::Normal);
+    });
+    connect(ui->largeRadioButton, &QRadioButton::toggled, this, [this](bool checked) {
+        if (checked) emit patientSizeChanged(PatientSize::Large);
+    });
 }
 
 BodySizeWidget::~BodySizeWidget()
 {
     delete ui;
+}
+
+void BodySizeWidget::setPatientSize(PatientSize size)
+{
+    switch (size) {
+        case PatientSize::Infant:
+            ui->infantRadioButton->setChecked(true);
+            break;
+        case PatientSize::Small:
+            ui->smallRadioButton->setChecked(true);
+            break;
+        case PatientSize::Normal:
+            ui->normalRadioButton->setChecked(true);
+            break;
+        case PatientSize::Large:
+            ui->largeRadioButton->setChecked(true);
+            break;
+    }
+}
+
+PatientSize BodySizeWidget::getPatientSize() const
+{
+    if (ui->infantRadioButton->isChecked()) return PatientSize::Infant;
+    if (ui->smallRadioButton->isChecked()) return PatientSize::Small;
+    if (ui->normalRadioButton->isChecked()) return PatientSize::Normal;
+    if (ui->largeRadioButton->isChecked()) return PatientSize::Large;
+
+    // Default to Normal if nothing is checked
+    return PatientSize::Normal;
 }
--- View/Widget/BodySizeWidget.h
+++ View/Widget/BodySizeWidget.h
@@ -7,6 +7,13 @@ namespace Ui {
 class BodySizeWidget;
 }
 
+enum class PatientSize {
+    Infant,
+    Small,
+    Normal,
+    Large
+};
+
 class BodySizeWidget : public QWidget
 {
     Q_OBJECT
@@ -14,6 +21,25 @@ class BodySizeWidget : public QWidget
 public:
     explicit BodySizeWidget(QWidget *parent = nullptr);
     ~BodySizeWidget();
+
+    /**
+     * @brief Sets the current patient size selection.
+     * @param size The patient size to select.
+     */
+    void setPatientSize(PatientSize size);
+
+    /**
+     * @brief Gets the currently selected patient size.
+     * @return The selected patient size.
+     */
+    PatientSize getPatientSize() const;
+
+signals:
+    /**
+     * @brief Emitted when the patient size selection changes.
+     * @param size The newly selected patient size.
+     */
+    void patientSizeChanged(PatientSize size);
 
 private:
     Ui::BodySizeWidget *ui;
