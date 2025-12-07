76734093de09619196207f081e8f71d49fc6cd07
altay-brusan <altay_bn@yahoo.com>
2025-12-07 14:58:53 +0300

    Add setter methods for patient demographics in UI
    Added setter methods (`setPatientName`, `setPatientId`,
    `setGender`, `setAge`, `setAccessionNumber`) to the
    `ExamTitleWidget` class for updating patient demographic
    information in the UI. Updated the header file to declare
    these methods and implemented their functionality in the
    source file to update corresponding labels in the UI.

--- View/Widget/ExamTitleWidget.cpp
+++ View/Widget/ExamTitleWidget.cpp
@@ -12,3 +12,28 @@ ExamTitleWidget::~ExamTitleWidget()
 {
     delete ui;
 }
+
+void ExamTitleWidget::setPatientName(const QString& name)
+{
+    ui->patientNameValueLbl->setText(name);
+}
+
+void ExamTitleWidget::setPatientId(const QString& id)
+{
+    ui->patientIdValueLbl->setText(id);
+}
+
+void ExamTitleWidget::setGender(const QString& gender)
+{
+    ui->genderValueLbl->setText(gender);
+}
+
+void ExamTitleWidget::setAge(int age)
+{
+    ui->ageValueLbl->setText(QString::number(age));
+}
+
+void ExamTitleWidget::setAccessionNumber(const QString& accessionNumber)
+{
+    ui->accessionNumberValueLbl->setText(accessionNumber);
+}
