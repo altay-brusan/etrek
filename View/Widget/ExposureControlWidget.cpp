1515fab162d990f2ff1dadf81247b2fb43b6d931
altay-brusan <altay_bn@yahoo.com>
2025-12-07 14:59:39 +0300

    Add exposure parameter handling to widget
    Enhanced `ExposureControlWidget` with new functionality:
    - Connected spin box `valueChanged` signals to emit `parametersChanged`.
    - Added `setExposureParameters` to set all parameters at once.
    - Introduced individual setters (`setKvp`, `setMa`, `setTime`, `setMas`).
    - Added getters (`getKvp`, `getMa`, `getTime`, `getMas`) for parameter retrieval.
    - Declared `parametersChanged` signal to notify parameter updates.
    - Updated class documentation to reflect new methods and signal.

--- View/Widget/ExposureControlWidget.cpp
+++ View/Widget/ExposureControlWidget.cpp
@@ -6,9 +6,71 @@ ExposureControlWidget::ExposureControlWidget(QWidget *parent)
     , ui(new Ui::ExposureControlWidget)
 {
     ui->setupUi(this);
+
+    // Connect spin boxes to emit parametersChanged signal
+    connect(ui->kvpSpinBox, QOverload<int>::of(&QSpinBox::valueChanged), this, [this]() {
+        emit parametersChanged(getKvp(), getMa(), getTime(), getMas());
+    });
+    connect(ui->maSpinBox, QOverload<int>::of(&QSpinBox::valueChanged), this, [this]() {
+        emit parametersChanged(getKvp(), getMa(), getTime(), getMas());
+    });
+    connect(ui->timeSpinBox, QOverload<int>::of(&QSpinBox::valueChanged), this, [this]() {
+        emit parametersChanged(getKvp(), getMa(), getTime(), getMas());
+    });
+    connect(ui->masSpinBox, QOverload<int>::of(&QSpinBox::valueChanged), this, [this]() {
+        emit parametersChanged(getKvp(), getMa(), getTime(), getMas());
+    });
 }
 
 ExposureControlWidget::~ExposureControlWidget()
 {
     delete ui;
+}
+
+void ExposureControlWidget::setExposureParameters(int kvp, int ma, int time, int mas)
+{
+    ui->kvpSpinBox->setValue(kvp);
+    ui->maSpinBox->setValue(ma);
+    ui->timeSpinBox->setValue(time);
+    ui->masSpinBox->setValue(mas);
+}
+
+void ExposureControlWidget::setKvp(int kvp)
+{
+    ui->kvpSpinBox->setValue(kvp);
+}
+
+void ExposureControlWidget::setMa(int ma)
+{
+    ui->maSpinBox->setValue(ma);
+}
+
+void ExposureControlWidget::setTime(int time)
+{
+    ui->timeSpinBox->setValue(time);
+}
+
+void ExposureControlWidget::setMas(int mas)
+{
+    ui->masSpinBox->setValue(mas);
+}
+
+int ExposureControlWidget::getKvp() const
+{
+    return ui->kvpSpinBox->value();
+}
+
+int ExposureControlWidget::getMa() const
+{
+    return ui->maSpinBox->value();
+}
+
+int ExposureControlWidget::getTime() const
+{
+    return ui->timeSpinBox->value();
+}
+
+int ExposureControlWidget::getMas() const
+{
+    return ui->masSpinBox->value();
 }
--- View/Widget/ExposureControlWidget.h
+++ View/Widget/ExposureControlWidget.h
@@ -15,6 +15,37 @@ public:
     explicit ExposureControlWidget(QWidget *parent = nullptr);
     ~ExposureControlWidget();
 
+    /**
+     * @brief Sets the exposure parameters.
+     * @param kvp Kilovolt peak value.
+     * @param ma Milliamperes value.
+     * @param time Exposure time in milliseconds.
+     * @param mas Milliampere-seconds value.
+     */
+    void setExposureParameters(int kvp, int ma, int time, int mas);
+
+    /**
+     * @brief Sets individual parameter values.
+     */
+    void setKvp(int kvp);
+    void setMa(int ma);
+    void setTime(int time);
+    void setMas(int mas);
+
+    /**
+     * @brief Gets current parameter values.
+     */
+    int getKvp() const;
+    int getMa() const;
+    int getTime() const;
+    int getMas() const;
+
+signals:
+    /**
+     * @brief Emitted when exposure parameters change.
+     */
+    void parametersChanged(int kvp, int ma, int time, int mas);
+
 private:
     Ui::ExposureControlWidget *ui;
 };
